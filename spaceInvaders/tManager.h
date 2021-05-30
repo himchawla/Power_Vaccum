6b70a6fd0493f189b02da8f0f7d82774990ccbd4
Bluesapphire04 <gervince.21@gmail.com>
2021-05-31 10:36:18 +1200

    Update TManager to be faster

--- spaceInvaders/tManager.h
+++ spaceInvaders/tManager.h
@@ -57,11 +57,11 @@ private:
 
 	// Independent Tile Manager Fall Timer
 	float m_fFallTimer = 0.0f;
-	float m_fTimeToFall = 7.0f;
+	float m_fTimeToFall = 5.5f;
 
-	float m_fSpeedIncrease = 0.6f;
-	const float m_cfFallDefault = 8.0f;
-	float m_fMaxFallTimer = 2.5f;
+	float m_fSpeedIncrease = 0.8f;
+	const float m_cfFallDefault = 6.0f;
+	float m_fMaxFallTimer = 1.5f;
 	int m_iTimesFallen = 0; // How Many Tiles have fallen.
 
 	// Shaking Animation.
